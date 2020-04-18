2db6a528bcfbe2ee370abdd3bb0f6c0b4ad533b2
Andy Sanchez <andysanchez726@gmail.com>
2020-04-17 21:56:12 -0500

    able to edit event blocks

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -14,3 +14,29 @@ setInterval(function() {
     }
 }, 1000);
 
+// add event to time blocks
+ 
+//on click edit time block
+$(".container").on("click", "#event", function() {
+    var text = $(this)
+        .text()
+        .trim();
+    var textInput = $("<textarea>")
+        .addClass("col")
+        .val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+ });
+
+// on blue textarea goes back to <p>
+$(".container").on("blur", "textarea", function() {
+    var text = $(this)
+        .val()
+        .trim();
+    console.log(text)
+    var eventP = $("<p>")
+        .addClass("col")
+        .attr("id", "event")
+        .text(text)
+    $(this).replaceWith(eventP);
+});
