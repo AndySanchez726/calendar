47506b413871e7a5d483162ae45a27f3028f3ccf
Andy Sanchez <andysanchez726@gmail.com>
2020-04-17 19:44:36 -0500

    added date and time to top of the page

---
+++ assets/js/calendar.js
@@ -0,0 +1,15 @@
+
+
+// current day and time
+var currentDate = function () {
+    var date = moment().format("MMMM Do YYYY hh:mm:ss");
+    $("#currentDay").remove("p");
+    $("header").append('<p id="currentDay" class="lead"></p>')
+    $("#currentDay").append(date);
+};
+currentDate();
+setInterval(function() {
+    if ( $("#currentDay").html > moment().format("MMMM Do YYYY hh:mm:ss")) {
+        currentDate();
+    }
+}, 1000);
